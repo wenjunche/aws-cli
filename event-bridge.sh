6cf1ac26fbecde30544d11fa143c1b4115158d77
Wenjun Che <wenjun@openfin.co>
2022-08-06 19:30:15 -0400

    fixed remove-targets

--- event-bridge.sh
+++ event-bridge.sh
@@ -2,4 +2,4 @@ aws events list-rules
 
 aws events list-targets-by-rule --rule every_hour
 
-aws events remove-targets --ids Id93ee06f9-d4d9-4e45-bf22-b87498aae92c
+aws events remove-targets --rule end_of_day --ids Id93ee06f9-d4d9-4e45-bf22-b87498aae92c
