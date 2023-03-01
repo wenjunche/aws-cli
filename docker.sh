9fd4d4733448e07c849f8ad8f6c155c94cbe9fb0
Wenjun Che <wenjun@openfin.co>
2023-03-01 16:02:57 -0500

    added docker

---
+++ docker.sh
@@ -0,0 +1,4 @@
+
+docker build . -f dockerfile  -t image-name --build-arg AWS_ACCESS_KEY_ID=asdf1234 --build-arg AWS_SECRET_ACCESS_KEY=asdf1234  --build-arg AWS_REGION=us-east-1
+
+docker run -it image-name  /bin/bash
