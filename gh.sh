38fb3933a73cd0fe42b87fcdee398c662b9e509a
Wenjun Che <wenjun@openfin.co>
2023-03-07 16:14:41 -0500

    added gh.sh

---
+++ gh.sh
@@ -0,0 +1,7 @@
+#add to workflow
+#on:
+#   pull_request: 
+#
+# then create a PR
+
+gh workflow run workflow_name --ref branch-name
--- terraform.sh
+++ terraform.sh
@@ -13,3 +13,11 @@ terraform state show 'aws_cloudfront_distribution.proxy'
 terraform plan -target=aws_s3_bucket.testing_dashboard
 
 terraform.exe import aws_ecr_repository.images client-cert-test
+
+terraform.exe import aws_route53_record.dns  ZONE_ID_client-cert-test-dev.openfin.co_CNAME
+
+terraform.exe  import aws_lb.network[0] arn:asdf
+
+terraform.exe state rm aws_lb.network
+
+terraform.exe state rm aws_lb.network[0]
