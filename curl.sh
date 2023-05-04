7582f67cab2d7cf45d8459e5fcc702df589ac69d
Wenjun Che <wenjun@openfin.co>
2023-05-04 09:38:17 -0400

    added curl

---
+++ curl.sh
@@ -0,0 +1,4 @@
+# Checking HSTS header
+# https://www.namecheap.com/support/knowledgebase/article.aspx/9711/38/how-to-check-if-hsts-is-enabled/
+# strict-transport-security: max-age=63072000; includeSubDomains; preload
+curl -s -D- https://example.com
