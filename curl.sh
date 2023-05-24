318475a72349d62072fc6ef901326f2acf5beb75
Wenjun Che <wenjun@openfin.co>
2023-05-24 15:06:11 -0400

    update elasticsearch.sh

--- curl.sh
+++ curl.sh
@@ -2,3 +2,7 @@
 # https://www.namecheap.com/support/knowledgebase/article.aspx/9711/38/how-to-check-if-hsts-is-enabled/
 # strict-transport-security: max-age=63072000; includeSubDomains; preload
 curl -s -D- https://example.com
+
+
+# upload multi-part files
+curl [URL] -F file1=@filename1 -F file2=@filename2
--- elasticsearch.sh
+++ elasticsearch.sh
@@ -1,5 +1,8 @@
 
-_alias/analytics.runtime.event
+_template/active-analytics.rvm.event  (legacy)
+_index_template/active-analytics.runtime.event
+
+_alias/analytics.rvm.event
 
 _cat/indices/active-11064fee744de7e080cc8b11eaac99468f3d4847-1
 
@@ -7,11 +10,68 @@ analytics.rvm.event/_count
 
 analytics.rvm.event/_settings
 
+active-f3a8035fc59fbffaa4756935075958e19972ed38-1/_ilm/explain
+
+# life cycle management 
+analytics.rvm.event/_ilm/explain
+
 # show mapping of the index
 analytics.rvm.event/_mapping/?pretty
 
 # show mapping of one field
 analytics.rvm.event/_mapping/field/event.payload.appConfig?pretty
+
+# show count of docs for a date range
+analytics.rvm.event/_search
+{
+  "query": {
+    "range": {
+      "timestamp": {
+        "gte": "2020/07/01",
+        "lt": "2020/07/15",
+        "format": "yyyy/MM/dd"
+      }
+    }
+  },
+  "fields": ["timestamp"],
+  "_source": false,
+  "size": 0,
+  "track_total_hits": true    
+}
+
+# search by date range.  this query returns a task
+GET analytics.rvm.event/_search
+{
+  "query": {
+    "range": {
+      "timestamp": {
+        "gte": "2020/07/01",
+        "lt": "2020/07/15",
+        "format": "yyyy/MM/dd"
+      }
+    }
+  }
+}
+
+# delete by date range.  this query returns a task
+POST analytics.rvm.event/_delete_by_query?wait_for_completion=false
+{
+  "query": {
+    "range": {
+      "timestamp": {
+        "gte": "2020/07/01",
+        "lt": "2020/07/15",
+        "format": "yyyy/MM/dd"
+      }
+    }
+  }
+}
+
+# query task by ID
+_tasks/pkTbKeM8TmKlXSUFp-dFnw:254410228
+
+# search for tasks by action
+_tasks?detailed=true&actions=*/delete/byquery
 
 # update mapping of one field
 PUT analytics.rvm.event/_mapping?pretty
@@ -37,3 +97,6 @@ Body:
 # Ports
 #   9200/tcp:  0.0.0.0:9200
 #   9300/tcp:  0.0.0.0:9300
+
+# delete an alias
+curl -X "DELETE" http://localhost:9200/*/_alias/analytics.rvm.event
