4b7c6f32731e9088166456bcdeb9efaae997181a
Wenjun Che <wenjun@openfin.co>
2023-07-06 16:17:00 -0400

    updated mfa delete

--- s3-enable-mfa-delete.sh
+++ s3-enable-mfa-delete.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/sh -x
+# SERIAL is serial number of the device
+# followed by code displayed on the device
 aws s3api put-bucket-versioning --bucket cdn.openfin.co --versioning-configuration Status=Enabled,MFADelete=Enabled --mfa "SERIAL 123456"
