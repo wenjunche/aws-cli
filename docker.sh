ea68266ff70748de341a6594c9322effc1a7d478
Wenjun Che <wenjun@openfin.co>
2023-07-13 15:48:13 -0400

    added docker save and load

--- docker.sh
+++ docker.sh
@@ -2,3 +2,9 @@
 docker build . -f dockerfile  -t image-name --build-arg AWS_ACCESS_KEY_ID=asdf1234 --build-arg AWS_SECRET_ACCESS_KEY=asdf1234  --build-arg AWS_REGION=us-east-1
 
 docker run -it image-name  /bin/bash
+
+#save image to a tar
+docker save -o output.tar  image-name:image-tag
+
+#load image from a tar
+docker load -i output.tar
--- ecr-login.sh
+++ ecr-login.sh
@@ -1,1 +1,4 @@
 aws ecr get-login-password --region  us-east-1  | docker login --username AWS --password-stdin [aws-account-id].dkr.ecr.us-east-1.amazonaws.com
+
+# find ARN of a repository
+aws ecr describe-repositories --profile=sandbox --repository-name=app-directory-api
