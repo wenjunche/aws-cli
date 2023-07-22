44ce22ef6bac2d5637a2b54e9e18c36239f16be3
Wenjun Che <wenjun@openfin.co>
2023-07-21 21:35:08 -0400

    update ecr

--- ecr-login.sh
+++ ecr-login.sh
@@ -1,4 +1,7 @@
 aws ecr get-login-password --region  us-east-1  | docker login --username AWS --password-stdin [aws-account-id].dkr.ecr.us-east-1.amazonaws.com
 
+# log in with different profile
+aws ecr get-login-password --profile sandbox  | docker login --username AWS --password-stdin {sandbox-account-id}.dkr.ecr.us-west-2.amazonaws.com
+
 # find ARN of a repository
 aws ecr describe-repositories --profile=sandbox --repository-name=app-directory-api
